8e7e39bcfc6b0289dc7632356f0fb15e960065f7
vaishnaviranpise <156096994+vaishnaviranpise@users.noreply.github.com>
2024-09-25 10:27:53 +0530

    Add files via upload

---
+++ country.py
@@ -0,0 +1,16 @@
+Class Country:
+    def AcceptCountry(self):
+self.cname=input("enter country name:")
+    def DisplayCountry(self):
+                print("country name is:",self cname)
+class state(country):
+    def AcceptState(self):
+self.sname=input("enter state name:")
+    def Display state(self):
+               print("state name is:",self sname)
+obj=state()
+obj.AcceptCountry()
+obj.AccceptState()
+obj.DisplayCountry()
+obj.DisplayState()
+                
