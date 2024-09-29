ee485c71a43465de9098c90e4bac65fbf23796e0
vaishnaviranpise <156096994+vaishnaviranpise@users.noreply.github.com>
2024-09-29 15:47:20 +0530

    Add files via upload

---
+++ lec2.py
@@ -0,0 +1,7 @@
+#WAP to input users first name & print its length.
+name = input("enter your name : ")
+print("length of your name is", len(name))
+
+#WAP to find the occurrence of '$' in a string.
+str = "Hi, $I am the $ symbol $99.99"
+print(str.count("$"))
